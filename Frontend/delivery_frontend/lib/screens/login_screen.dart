6f067711b1fa461ca1e913875b3a8ca13f45ebc5
Kaan-1 <kerdogan20@ku.edu.tr>
2024-05-17 12:05:37 +0300

    modified:   Frontend/delivery_frontend/lib/main.dart 	new file:   Frontend/delivery_frontend/lib/screens/login_screen.dart 	modified:   Frontend/delivery_frontend/pubspec.lock

---
+++ Frontend/delivery_frontend/lib/screens/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login or Register'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle login
+              },
+              child: Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle registration
+              },
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
