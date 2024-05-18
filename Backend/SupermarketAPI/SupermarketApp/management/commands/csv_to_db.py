ec8a44a3f9f93d6f5fe158a9d77544a7edd267dc
omeratasoy <oatasoy19@ku.edu.tr>
2024-05-18 20:19:32 +0300

    Add auto id from csv support

--- Backend/SupermarketAPI/SupermarketApp/management/commands/csv_to_db.py
+++ Backend/SupermarketAPI/SupermarketApp/management/commands/csv_to_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 import csv
-from SupermarketApp.dao import executeRaw
+from SupermarketApp.dao import executeRaw, get_next_id
 
 
 class Command(BaseCommand):
@@ -10,6 +10,7 @@ class Command(BaseCommand):
         parser.add_argument('table_name', type=str, help="Table name")
         parser.add_argument('path', type=str, help="Path to csv file")
         parser.add_argument("--truncate", action="store_true", help="Truncate table before import")
+        parser.add_argument("auto_id_column", nargs=1, default=None, help="Auto id column name of table (if any)")
 
     def handle(self, *args, **options):
         table_name = options['table_name']
@@ -21,6 +22,8 @@ class Command(BaseCommand):
             reader = csv.reader(csv_data)
             csv_data_list = list(reader)
             for row in csv_data_list:
+                if options['auto_id_column']:
+                    row.insert(0, get_next_id(table_name, options['auto_id_column'][0]))
                 row = tuple(row)
                 executeRaw(f"INSERT INTO {table_name} VALUES {row}")
                 
