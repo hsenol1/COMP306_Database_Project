7b9c7f197ba1fed4ba6585be0af2fe69e3042da0
hsenol1 <hsenol19@ku.edu.tr>
2024-06-06 09:55:28 +0300

    asign random vouchers

--- Backend/SupermarketAPI/SupermarketApp/urls.py
+++ Backend/SupermarketAPI/SupermarketApp/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('create-order/', views.create_order, name='create_order'),
     path('get-last-10-orders/', views.get_last_10_orders, name='get-last-10-orders'),
     path('login-user/', views.login_user, name='login_user'),
+    path('assign-random-vouchers/',views.assign_random_vouchers, name ='assign_random_vouchers')
 ]
